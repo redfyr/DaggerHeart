f1c23b63c58b78c7f7cfb2ddeab73b145c4e882e
Red <red.fyr.org@gmail.com>
2025-12-06 03:41:39 +1100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 // REPLACE 'daggerheart' with whatever you name your GitHub repository!
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
+    const env = loadEnv(mode, process.cwd(), '');
     return {
       base: '/DaggerHeart/', 
       server: {
